05edb287a21f370fd70c523141f1fea00bbc338e
Rohan Sharda <rohansharda2001@gmail.com>
2024-02-25 16:40:56 +0530

    update: added ping route for test

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -6,5 +6,6 @@ const appRouter = Router();
 
 appRouter.use("/user", userRoutes); //domain/api/v1/user
 appRouter.use("/chat", chatRoutes); //domain/api/v1/chats
+appRouter.get("/ping", async(req, res) => res.send("Pong!"))
 
 export default appRouter;
